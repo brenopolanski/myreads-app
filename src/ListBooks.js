ebd8afb97d80cd0a64dd7887f1b37221130f7fc0
Thales Carvalho <thales.comp@gmail.com>
2017-07-19 00:58:44 -0300

    Convert ListBooks to stateless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,34 +3,29 @@ import { Link } from 'react-router-dom'
 import { PropTypes } from 'prop-types'
 import Bookshelf from './Bookshelf.js'
 
-class ListBooks extends Component {
-  render() {
-    const {booksOnShelves, bookshelves, onChangeShelf} = this.props
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {bookshelves.map((bookshelf) => (
-              <Bookshelf 
-                key={bookshelf.key}
-                bookshelfTitle={bookshelf.title}
-                bookshelves={bookshelves}
-                booksOnShelves={booksOnShelves.filter(book => book.shelf === bookshelf.key)}
-                onChangeShelf={onChangeShelf}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+const ListBooks = ({booksOnShelves, bookshelves, onChangeShelf}) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      <div>
+        {bookshelves.map((bookshelf) => (
+          <Bookshelf 
+            key={bookshelf.key}
+            bookshelfTitle={bookshelf.title}
+            bookshelves={bookshelves}
+            booksOnShelves={booksOnShelves.filter(book => book.shelf === bookshelf.key)}
+            onChangeShelf={onChangeShelf}
+          />
+        ))}
       </div>
-    )
-  }
-}
+    </div>
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  </div>
+)
 
 ListBooks.propTypes = {
   booksOnShelves: PropTypes.array.isRequired,
